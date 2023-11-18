ff6adaea3c4b4f986631577401f6aca64079af01
samantha <samanthap8675@gmail.com>
2023-11-18 16:01:21 -0500

    update

---
+++ Frontend/medicalApp/CalendarCell.swift
@@ -0,0 +1,14 @@
+//
+//  CalendarCell.swift
+//  medicalApp
+//
+//  Created by sam pang on 11/18/23.
+//
+
+import Foundation
+import UIKit
+
+class CalendarCell: UICollectionViewCell {
+    @IBOutlet weak var dayOfMonth: UILabel!
+    
+}
