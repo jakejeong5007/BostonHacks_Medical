c614523efcc7a4af0a8ee2f30065df496690675a
jakejeong <jake50071227@gmail.com>
2023-11-18 15:10:39 -0500

    v1

--- Backend/base.py
+++ Backend/base.py
@@ -1,30 +1,10 @@
 from openai import OpenAI
 import json
+import config
 
-
-
-info = input("User information\n")
-
-completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are a medical assistant \
-      that gives food recommendation depending on the medcial information of the user.\
-      Please give an answer by listing three food recommendation and a sentence explanation for each of them"},
-    {"role": "user", "content": info}
-  ]
-)
-
-
-response = completion.choices[0].message.content.split("\n")
-
-for row in response:
-  if len(row) > 1:
-    print(row)
-    
 
 def recommend_food(u_info):
-  client = OpenAI(api_key = "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")
+  client = OpenAI(api_key = config.OPENAI_API_KEY)
   completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
